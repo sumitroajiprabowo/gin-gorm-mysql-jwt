3a8a5b9f68cdec8d8a1364684973b61d7bbdbf06
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-03-10 18:52:25 +0700

    Update login dto

--- dto/login_dto.go
+++ dto/login_dto.go
@@ -2,6 +2,6 @@ package dto
 
 // Create Login DTO Request Struct when user login from /login URL
 type LoginDTORequest struct {
-	Email    string `json:"email" form:"email" binding:"required" validate:"email"`
-	Password string `json:"password,omnitempty" form:"password,omnitempty" binding:"required"`
+	Email    string `json:"email" form:"email" binding:"required, email"`
+	Password string `json:"password" form:"password" binding:"required,min=8"`
 }
