e454122c03aa931ae26c8db9bd47f6d8e82f0478
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-03-10 18:51:40 +0700

    Add var

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/sumitroajiprabowo/gin-gorm-jwt-mysql/config"
 	"github.com/sumitroajiprabowo/gin-gorm-jwt-mysql/controllers"
+	"github.com/sumitroajiprabowo/gin-gorm-jwt-mysql/repository"
+	"github.com/sumitroajiprabowo/gin-gorm-jwt-mysql/services"
 	"gorm.io/gorm"
 )
 
 var (
-	db             *gorm.DB = config.SetupDatabase()
-	authController          = controllers.NewAuthController()
+	db             *gorm.DB                  = config.SetupDatabase()
+	userRepository repository.UserRepository = repository.NewUserRepository(db)
+	jwtService     services.JWTService       = services.NewJWTService()
+	authService    services.AuthService      = services.NewAuthService(userRepository)
+	authController                           = controllers.NewAuthController(authService, jwtService)
 )
 
 func main() {
