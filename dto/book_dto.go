2ee1a4b4f3b0db2377dfbb662f0b7466df3311e7
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-03-11 14:22:59 +0700

    Update book dto

--- dto/book_dto.go
+++ dto/book_dto.go
@@ -2,12 +2,12 @@ package dto
 
 // Create Book Update DTO Request when user update book
 type BookUpdateDTORequest struct {
-	Id          uint64 `json:"id" form:"id" binding:"required"`
+	ID          uint64 `json:"id" form:"id" binding:"required"`
 	Title       string `json:"title" form:"title" binding:"required"`
 	Author      string `json:"author" form:"author" binding:"required"`
 	Price       int64  `json:"price" form:"price" binding:"required"`
 	Description string `json:"description" form:"description" binding:"required"`
-	UserId      uint64 `json:"user_id,omnitempty" form:"user_id" binding:"required"`
+	UserID      uint64 `json:"user_id,omnitempty" form:"user_id" binding:"required"`
 }
 
 // Create Book Create DTO Request when user create book
@@ -16,5 +16,5 @@ type BookCreateDTORequest struct {
 	Author      string `json:"author" form:"author" binding:"required"`
 	Price       int64  `json:"price" form:"price" binding:"required"`
 	Description string `json:"description" form:"description" binding:"required"`
-	UserId      uint64 `json:"user_id,omnitempty" form:"user_id" binding:"required"`
+	UserID      uint64 `json:"user_id,omnitempty" form:"user_id" binding:"required"`
 }
