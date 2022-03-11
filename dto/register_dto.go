e2b55198d5a64e29e7a87b64b07799022b316c1d
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-03-11 14:39:04 +0700

    Fix validate

--- dto/register_dto.go
+++ dto/register_dto.go
@@ -3,7 +3,7 @@ package dto
 // Create Register DTO Request Struct when user register from /register URL
 type RegisterDTORequest struct {
 	// Name is the name of the user with minimum length of 3 characters and maximum length of 100 characters
-	Name string `json:"name" form:"name" binding:"required, min=3, max=100"`
+	Name string `json:"name" form:"name" binding:"required,min=3,max=100"`
 	// Email is the email of the user with regular expression of email format and required
 	Email string `json:"email" form:"email" binding:"required,email"`
 	// Password is the password of the user with minimum length of 8 characters and maximum length of 100 characters
