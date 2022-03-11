ac739f56eb77d7b79dfaf479c90f3af6feeca549
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-03-11 14:38:47 +0700

    Update user update dto

--- dto/user_dto.go
+++ dto/user_dto.go
@@ -2,8 +2,8 @@ package dto
 
 // Create User Update DTO Request Struct when user update profile
 type UserUpdateDTORequest struct {
-	Id       uint64 `json:"id" form:"id"`
-	Email    string `json:"email" form:"email" binding:"required"`
-	Name     string `json:"name" form:"name" binding:"required,email"`
+	ID       uint64 `json:"id" form:"id"`
+	Name     string `json:"name" form:"name" binding:"required"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
 	Password string `json:"password,omitempty" form:"password,omitempty"`
 }
